4613f035d75d1c752e5d45651d6fc3d3407e856b
Khoroshikh Arkadiy <khorark@gmail.com>
2020-03-04 14:03:36 +0300

    Added func nom2Store and norm2Server

---
+++ src/index.d.ts
@@ -0,0 +1,2 @@
+export const norm2Store = (data: any, scheme: Array<[string, string?, any?]>): any => {};
+export const norm2Server = (data: any, scheme: Array<[string, string?, any?]>): any => {};
---
+++ src/index.js
@@ -0,0 +1,27 @@
+export const norm2Store = (data, scheme) => {
+  const calcValue = (v, d) =>
+    v.split(".").reduce((resV, vv) => (resV = resV[vv]), d);
+
+  return scheme.reduce((res, value) => {
+    const resV = calcValue(value[0], data);
+    const arrFields = (value[1] || value[0]).split(".");
+
+    arrFields.reduce((resObj, vv, idx) => {
+      if (idx === arrFields.length - 1) {
+        resObj[vv] = resV === undefined ? value[2] : resV;
+        return resObj;
+      } else {
+        resObj[vv] = {};
+        return resObj[vv];
+      }
+    }, res);
+
+    return res;
+  }, {});
+};
+
+export const norm2Server = (data, scheme) =>
+  getResult(
+    data,
+    scheme.map(value => (value[1] ? [value[1], value[0]] : [value[0]]))
+  );
